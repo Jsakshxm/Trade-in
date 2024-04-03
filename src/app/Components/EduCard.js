d3fe99236ffe2436cb6f3c45d2cbc5de95331c51
Saksham jain <130843255+Jsakshxm@users.noreply.github.com>
2024-04-03 16:26:51 +0530

    Edu Topics

--- src/app/Components/EduCard.js
+++ src/app/Components/EduCard.js
@@ -1,17 +1,19 @@
 import React from 'react';
 
-const EduCard = ({ imgSrc, title, description, percentage,read }) => {
+const EduCard = ({ imgSrc, title, description, percentage }) => {
   return (
-    <div className='flex items-center p-4 m-2 bg-white rounded-lg shadow-md'>
-      <img src={imgSrc} alt={title} className='w-16 h-16 mr-4 rounded-full' />
-      <div>
-        <h2 className='text-xl font-semibold'>{title}</h2>
-        <p className='text-gray-600'>{description}</p>
-        <div className='w-full h-2 mt-2 bg-gray-200 rounded-full'>
-          <div className='h-2 bg-blue-500 rounded-full' style={{ width: `${percentage}%` }}></div>
-          <div></div>
+    <div className="flex flex-col justify-between max-w-sm p-4 m-2 bg-white rounded-lg shadow-md">
+      <div className="flex items-center">
+        <img src={imgSrc} alt={title} className="w-16 h-16 mr-4 rounded-full" />
+        <div>
+          <h2 className="text-lg font-semibold">{title}</h2>
+          <p className="text-sm text-gray-600">{description}</p>
         </div>
       </div>
+      <div className="w-full h-2 mt-4 bg-gray-200 rounded-full">
+        <div className="h-2 bg-blue-500 rounded-full" style={{ width: `${percentage}%` }}></div>
+      </div>
+      <div className="mt-2 text-xs text-gray-500">{percentage}% Complete</div>
     </div>
   );
 };
---
+++ src/app/utils/topics.js
@@ -0,0 +1,36 @@
+const topics = [
+    {
+      title: "Introduction to Stock Markets",
+      description: "Understanding the basics of stock markets, including how they function, the role of exchanges, and the buying and selling process."
+    },
+    {
+      title: "Technical Analysis",
+      description: "Examining historical price and volume data to predict future price movements and make trading decisions."
+    },
+    {
+      title: "Fundamental Analysis",
+      description: "Assessing a company's intrinsic value by analyzing financial statements, industry trends, and economic factors."
+    },
+    {
+      title: "Currency, Commodity, and Government Securities",
+      description: "Exploring different asset classes beyond stocks, including currencies, commodities, and government securities, and understanding their role in diversified portfolios."
+    },
+    {
+      title: "Risk Management and Trading Psychology",
+      description: "Identifying and managing risks associated with investing, as well as understanding the psychological factors that influence trading decisions."
+    },
+    {
+      title: "Personal Finance - Mutual Funds",
+      description: "Exploring mutual funds as an investment vehicle, including their types, benefits, and considerations for building a diversified portfolio."
+    },
+    {
+      title: "Sector Analysis",
+      description: "Evaluating specific sectors or industries within the market to identify trends, opportunities, and risks for investment."
+    },
+    {
+      title: "Markets and Taxation",
+      description: "Understanding the tax implications of investment activities and how to optimize tax strategies within financial markets."
+    }
+  ];
+  
+  export default topics;

9d778edca4856795b1822ec8d8fb59329ffd5ca6
Raj Rawariya <98628762+Raj100@users.noreply.github.com>
2024-04-03 18:07:59 +0530

    added eduation and utils

--- src/app/Components/EduCard.js
+++ src/app/Components/EduCard.js
@@ -1,3 +1,27 @@
+<<<<<<< HEAD
+import React from 'react';
+
+const EduCard = ({ imgSrc, title, description, percentage }) => {
+  return (
+    <div className="float float-col relative justify-between max-w p-5 m-3 bg-white rounded-lg shadow-md overflow-x-auto overflow-y-auto">
+
+      <div className="float items-center">
+        <img src={imgSrc} alt={title} className="w-28 h-28 mr-4 rounded-full allign-item-center justify-center" />
+        <div>
+          <h2 className="text-lg font-semibold">{title}</h2>
+          <p className="text-sm text-gray-600">{description}</p>
+        </div>
+      </div>
+      <div className="w-full h-2 mt-4 bg-gray-200 rounded-full">
+        <div className="h-2 bg-blue-500 rounded-full" style={{ width: `${percentage}%` }}></div>
+      </div>
+      <div className="mt-2 text-xs text-gray-500">{percentage}% Complete</div>
+    </div>
+  );
+};
+
+export default EduCard;
+=======
 import React from 'react';
 
 const EduCard = ({ imgSrc, title, description, percentage }) => {
@@ -20,3 +44,4 @@ const EduCard = ({ imgSrc, title, description, percentage }) => {
 };
 
 export default EduCard;
+>>>>>>> 7086b897c88c23a8e0e438d31bdc733b5e5c42e8
--- src/app/Education/page.js
+++ src/app/Education/page.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react';
 import EduNav from '../Components/EduNav';
 import EduCard from '../Components/EduCard';
@@ -24,3 +25,31 @@ const Page = () => {
 };
 
 export default Page;
+=======
+import React from 'react';
+import EduNav from '../Components/EduNav';
+import EduCard from '../Components/EduCard';
+import { module_1 } from '../utils/info';
+import topics from '../utils/topics';
+
+const Page = () => {
+  return (
+    <div>
+      <EduNav />
+      <div className="flex overflow-x-auto">
+        {topics.map((topic, index) => (
+          <EduCard
+            key={index}
+            title={topic.title}
+            description={topic.description}
+            imgSrc={topic.imgSrc}
+            percentage={50} // Default percentage value
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Page;
+>>>>>>> 7086b897c88c23a8e0e438d31bdc733b5e5c42e8

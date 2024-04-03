701d467d80fb6e3140f56e4739000a067b736ab5
Raj Rawariya <98628762+Raj100@users.noreply.github.com>
2024-04-03 18:12:29 +0530

    fix

--- src/app/Education/page.js
+++ src/app/Education/page.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import EduNav from '../Components/EduNav';
 import EduCard from '../Components/EduCard';
@@ -25,31 +24,3 @@ const Page = () => {
 };
 
 export default Page;
-=======
-import React from 'react';
-import EduNav from '../Components/EduNav';
-import EduCard from '../Components/EduCard';
-import { module_1 } from '../utils/info';
-import topics from '../utils/topics';
-
-const Page = () => {
-  return (
-    <div>
-      <EduNav />
-      <div className="flex overflow-x-auto">
-        {topics.map((topic, index) => (
-          <EduCard
-            key={index}
-            title={topic.title}
-            description={topic.description}
-            imgSrc={topic.imgSrc}
-            percentage={50} // Default percentage value
-          />
-        ))}
-      </div>
-    </div>
-  );
-};
-
-export default Page;
->>>>>>> 7086b897c88c23a8e0e438d31bdc733b5e5c42e8
